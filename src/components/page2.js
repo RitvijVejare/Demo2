a326e0de9bd583dd92edc145399bc3a09f267cc4
RitvijVejare <47212534+RitvijVejare@users.noreply.github.com>
2021-06-06 19:58:09 +0530

    Add files via upload

--- src/components/page2.js
+++ src/components/page2.js
@@ -1,12 +1,43 @@
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Box, Button, Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from './navbar';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const Page2 = () => {
     return ( 
         <div>
             <Navbar />
-            <Typography>Page 2</Typography>
+            <div>
+                <Grid container>
+                    <Grid item xs={2} style={{backgroundColor:"#000", height:"800px"}}>
+                        <Grid container style={{paddingRight:"20px"}}>
+                            <Grid item xs={12} style={{margin:"20px 10px", display:"flex", flexDirection:"row", justifyContent:"flex-end"}}>
+                                <Link to="#" style={{textDecoration:"none"}}>
+                                    <Typography style={{fontSize:"15px", color:"gray"}}>
+                                        <ArrowBackIosIcon style={{fontSize:"10px", color:"gray"}} />Back
+                                    </Typography>
+                                </Link>
+                            </Grid>
+                            <Grid item xs={12} style={{display:"flex", flexDirection:"row", justifyContent:"flex-end"}}>
+                                <Typography variant="h6" style={{color:"#FFF", marginRight:"10px"}}>
+                                    Logic
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box border={1} style={{height:"800px"}}>
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <Typography style={{padding:"20px"}} variant="h6">Joseph, Kevin, and Nicholas are 3 brothers, and the following statements about them are all true:</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={4} />
+                </Grid>
+            </div>
         </div>
      );
 }
